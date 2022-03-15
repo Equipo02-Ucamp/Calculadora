727171fa2e298189df103d66739138f1744b5a54
victor_perezvaldez@hotmail.com <victor_perezvaldez@hotmail.com>
2022-03-15 11:43:32 -0600

    first commit

---
+++ app.js
@@ -0,0 +1,31 @@
+let numeros = [];
+
+
+
+pedirNumeros();
+calculadora();
+
+function pedirNumeros(){
+    for(let i = 0; i < 2; i++){
+        numeros[i] = parseInt(prompt('Inserte el numero'));
+    }
+}
+
+function calculadora(){
+    let operacion = prompt('Inserte el numero de la opcion\n1.Sumar\n2.Restar\n3.Multiplicar\n4.Dividir')
+    operacion = Number(operacion);
+    if(isNaN(operacion)){
+        alert('Escriba el numero de la opcion')
+        calculadora();
+    }
+    switch (operacion) {
+        case 1:
+            let total = numeros[0] + numeros[1];
+            alert(`El total de la suma es: ${total}`)
+            break;
+    
+        default:
+            break;
+    }
+
+}
